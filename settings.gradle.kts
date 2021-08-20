d8328e8f9954529effcec0ef685125028a784251
GitHub Action <action@github.com>
2021-08-20 11:03:48 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "eos_plus"
--- src/main/kotlin/com/github/abbywangxinhui/eosplus/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/abbywangxinhui/eosplus/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.abbywangxinhui.eosplus.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.abbywangxinhui.eosplus.MyBundle
 
 class MyApplicationService {
 
